915ca8305d76b934e8348fd7687ade78e7f039de
Shaaban Hamdy <119350285+ShaabanHamdy@users.noreply.github.com>
2022-12-10 02:55:21 +0200

    Update js 12.js

--- js/js 12.js
+++ js/js 12.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
   });
 });
 // =================================================
-let countDaownDate = new Date("November 30, 2029 23:59:59").getTime();
+let countDaownDate = new Date("January  1, 2023 23:59:59").getTime();
 
 let counter = setInterval(() => {
   let dateNow = new Date().getTime();
